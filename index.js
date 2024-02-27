74fcbd0e9414f36bdbc7cdfef9ec6ba368aae24a
Luucho <wittmundluciano@gmail.com>
2024-02-27 20:00:50 -0300

    Agregamos variables de entorno

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const http = require("http")
 
 function requestController (){
@@ -8,4 +9,8 @@ console.log("Recibimos una nueva request")
 //Configurar nuestro servidor
 const server = http.createServer(requestController)
 
-server.listen(4000)
+const PORT = process.env.PORT
+
+server.listen(PORT, function() {
+    console.log("Aplicacion corriendo en el puerto: " + PORT)
+})
